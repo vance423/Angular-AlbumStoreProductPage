15b3e5d6e260aa63666237cc49e0e7030b1f9b8e
vance <vancesoita2014@hotmail.com>
2021-11-18 19:02:28 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { Http, Response } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from './product.service';
-import { from } from 'rxjs';
+import {ProductService} from './product.service';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    ProductService
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
