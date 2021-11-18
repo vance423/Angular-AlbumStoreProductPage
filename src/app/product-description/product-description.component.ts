5bd08c0855ff028df9adda949f58acc6cad48091
vance <vancesoita2014@hotmail.com>
2021-11-18 18:02:50 +0300

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo!: Album;
+  albumInfo: Album | undefined;
 
   constructor(private _productService: ProductService) { }
 
