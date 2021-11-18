cd4b7f4064671053d48f38562751daa38a5497e5
vance <vancesoita2014@hotmail.com>
2021-11-18 17:12:51 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { Http, Response } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { map } from "rxjs/operators"; 
 
+import { map } from "rxjs/operators"; 
+import { Http ,Response } from '@angular/http'
+import { from } from 'rxjs';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: Http) { }
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).pipe(map((response: any) => response.json()));;
