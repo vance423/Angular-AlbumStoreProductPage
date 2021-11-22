542182d94192036309f3f96a4864b43439876ff1
vance <vancesoita2014@hotmail.com>
2021-11-22 14:11:19 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from './album';
 import { map, pipe } from 'rxjs';
 import 'rxjs/add/operator/map';
 
